e0c251091c3755eacd725c03c6fdfa333a2bdafe
Nora Stige <norastige@Nora-sin-MacBook-Pro.local>
2024-11-11 14:48:26 +0100

    finished logic in js

--- src/js/app.js
+++ src/js/app.js
@@ -10,9 +10,9 @@ const hoursContainer = document.querySelector(".timer_hours");
 const controlButtons = document.querySelectorAll("controls__button");
 
 // Selecting the buttons separatly as well
-const startButton = document.querySelector("controls__button--start");
-const pauseButton = document.querySelector("controls__button--pause");
-const resetButton = document.querySelector("controls__button--reset");
+const startButton = document.querySelector(".controls__button--start");
+const pauseButton = document.querySelector(".controls__button--pause");
+const resetButton = document.querySelector(".controls__button--reset");
 
 // Selecting the paragraph element
 const controlsGuide = document.querySelector(".stopwatch__guide");
@@ -50,17 +50,44 @@ minutesContainer.textContent = formattedMinutes;
 hoursContainer.textContent = formattedHours;
 };
 
-// Adding Event Listeners til buttons (hva som skjer når man trykker på knappene)
-startButton.addEventListener("click", handleControls);
-pauseButton.addEventListener("click", handleControls);
-resetButton.addEventListener("click", handleControls);
 
 // Passing the function ^ med hva som skjer når man trykker på knappene over. Samme funksjon for alle tre knapper. Bruker if statement for å vite hvilken knapp som er trykket.
 const handleControls = (e)=>{
-    if(e.target.classList.includes("controls__button-start")){ // Henter classname fra HTML
+    if(e.target.classList.contains("controls__button--start")){ // Henter classname fra HTML
+        // startButton.classList.remove("controls__button--start");
+        pauseButton.textContent = "Pause"; // Setter ordet tilbake til pause når den har vært på pause
+        startButton.textContent = "Start"; // Setter ordet tilbake til start etter man har trykket på resume
+        controlsGuide.style.visibility = "hidden"; // Skjuler resume melding
+
         if(timerStatus !== null){ // Sjekke om en intervall allerede løper. De kan ikke være i konflikt med hverandre. Man trenger alltid én aktiv intervall. Bruker if statement.
             clearInterval(timerStatus);
         }
        timerStatus = setInterval(handleIncrement, 10); //Setter intervall
+    } else if (e.target.classList.contains("controls__button--pause")){ // Etter man er ferdig å sjekke om start knappen går, sjekker vi nå videre til neste knapp
+        clearInterval(timerStatus); // Sier den nå stoppes dersom den går. 
+        pauseButton.textContent = "Paused";
+        startButton.textContent = "Resume";
+        controlsGuide.style.visibility = "visible";
+    } else if (e.target.classList.contains("controls__button--reset")){ // Sjekker her siste knapp
+        clearInterval(timerStatus); // Stopper den
+        hours = 0; // Resetter verdien til 0, slik at den ikke beholder det den allerede har telt opp
+        minutes = 0; 
+        seconds = 0; 
+        milliseconds = 0; 
+
+        millisecondsContainer.textContent = "000"; // Resetter det visuelle tallet som vises også
+        secondsContainer.textContent = "00"; 
+        minutesContainer.textContent = "00"; 
+        hoursContainer.textContent = "00"; 
+
+        pauseButton.textContent = "Pause";
+        startButton.textContent = "Start";
+        controlsGuide.style.visibility = "hidden";
     }
 }
+
+// Adding Event Listeners til buttons (hva som skjer når man trykker på knappene)
+// Må ligge etter logikken
+startButton.addEventListener("click", handleControls);
+pauseButton.addEventListener("click", handleControls);
+resetButton.addEventListener("click", handleControls);
